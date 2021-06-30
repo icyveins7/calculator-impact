b5f859e03ee7e599f4ab8945fb9604fd870e6a31
SeoLubuntu <icyveins@hotmail.com>
2021-06-30 12:00:03 +0800

    add settings json for resolution

--- app/ci_settingsframe.py
+++ app/ci_settingsframe.py
@@ -7,7 +7,9 @@ Created on Wed Jun  2 10:35:46 2021
 """
 
 from PySide2.QtWidgets import QFrame, QFormLayout, QComboBox
-from PySide2.QtCore import Signal, SLOT
+from PySide2.QtCore import Signal, Slot
+
+import json
 
 class SettingsFrame(QFrame):
     def __init__(self):
@@ -19,6 +21,16 @@ class SettingsFrame(QFrame):
         # Call all grouped settings forms
         self.resDropdown = self.imgSettingsForm()
         
+        # Load from file
+        try:
+            self.readSettings()
+        except:
+            # Create the settings file
+            self.writeSettings()
+        
+        # Connections
+        self.resDropdown.currentTextChanged.connect(self.writeSettings)
+        
     def imgSettingsForm(self):
         resDropdown = QComboBox()
         resDropdown.addItems(['2560x1440','1920x1080'])
@@ -29,3 +41,21 @@ class SettingsFrame(QFrame):
         
     def getResolution(self):
         return self.resDropdown.currentText()
+    
+    @Slot()
+    def writeSettings(self):
+        print("Saving settings")
+        with open("settings.json", "w") as f:
+           settings = {}
+           settings['resolution'] = self.getResolution()
+           
+           json.dump(settings, f)
+           
+    @Slot()
+    def readSettings(self):
+        print("Reading settings")
+        with open("settings.json", "r") as f:
+            settings = json.load(f)
+            print(settings)
+            
+            self.resDropdown.setCurrentText(settings['resolution'])
