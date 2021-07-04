a876a0afd1ddea6a68ceec3c927c2a2893d6fcf3
Lim Jing <limjing7@gmail.com>
2021-07-04 22:34:04 +0800

    auto capture v1 # cannot restart autocapture after stopping

--- app/ci_mainwindow.py
+++ app/ci_mainwindow.py
@@ -7,9 +7,9 @@ Created on Fri May 21 21:01:00 2021
 
 from PySide2.QtWidgets import QApplication
 from PySide2.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QLabel
-from PySide2.QtCore import QSize, QMimeData, QObject, Signal, Slot  
+from PySide2.QtCore import QSize, QMimeData, QObject, Signal, Slot, QCoreApplication  
 from PySide2.QtGui import QKeyEvent, QClipboard, QImage, QIcon, QDropEvent
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QThread
 
 from ci_artifactwidget import ArtifactWidget, FlowerWidget, FeatherWidget, TimepieceWidget, GobletWidget, HeadpieceWidget
 from ci_artifactlistwidget import ArtifactListWidget, ArtifactListFrame
@@ -22,6 +22,8 @@ import sys
 import os
 from skimage.io import imread
 # import matplotlib.pyplot as plt
+
+from screen_watcher import ScreenWatchWorker
 
 # sys.path.append(os.path.join(os.getcwd(), "..","cpp"))
 # sys.path.append(os.path.join(os.getcwd(), "..","scripts"))
@@ -40,6 +42,7 @@ from scripts.artifact import *
 
 class CIMainWindow(QMainWindow):
     # deselectFrameSignal = Signal()
+
     def __init__(self):
         super().__init__()
         
@@ -112,6 +115,9 @@ class CIMainWindow(QMainWindow):
         
         # Set droppable events
         self.setAcceptDrops(True)
+
+        # Autocaputre
+        autocapture = False
 
 
     @Slot()
@@ -211,6 +217,25 @@ class CIMainWindow(QMainWindow):
                     image = np.copy(image[:,:,:3], order='C') # need to copy into c-contiguous
                     qimg = QImage(image, image.shape[1], image.shape[0], QImage.Format_RGB888)
                     self.currentFrame.pasteImage(qimg)
+        
+        if (e.key() == Qt.Key_G and (e.modifiers() & Qt.ControlModifier)):
+            print("CTRL-G pressed")
+            print("Auto Capturing Enabled")
+        
+            self.autocapture = True
+            self.thread = QThread()
+            self.worker = ScreenWatchWorker(currentWidth, currentHeight, self.citaw, self.stop_fn)
+            self.worker.moveToThread(self.thread)
+            self.thread.started.connect(self.worker.run)
+            self.worker.new_artifact.connect(self.updateArtifact)
+        
+            self.thread.start()
+            
+        
+        if (e.key() == Qt.Key_H and (e.modifiers() & Qt.ControlModifier)):
+            print("CTRL-H pressed")
+            print("Auto Capturing Disabled")
+            self.autocapture = False
                     
     def dropEvent(self, e: QDropEvent):
         super().dropEvent(e)
@@ -220,3 +245,14 @@ class CIMainWindow(QMainWindow):
         mime = e.mimeData()
         if (mime.hasUrls()):
             print("mime has urls.")
+
+    def updateArtifact(self, new_artifact):
+        print('lala')
+        self.currentFrame.loadArtifactStats(new_artifact)
+        self.currentFrame.on_savebtn_pressed()
+    
+    def stop_fn(self):
+        if not self.autocapture:
+            return True
+        else:
+            return False
---
+++ app/screen_watcher.py
@@ -0,0 +1,62 @@
+from PySide2.QtCore import QObject, QThread, Signal, Slot
+
+import mss
+import numpy as np
+from PIL import Image, ImageQt
+
+import time
+
+from scripts.ocr_backend_combined import generate_dict
+from scripts.artifact import *
+
+class ScreenWatchWorker(QObject):
+    new_artifact = Signal(Artifact)
+
+    def __init__(self, width, height, tessapi_wrapper, stop_fn):
+        super(ScreenWatchWorker, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.screen_width = width
+        self.screen_height = height
+        self.citaw = tessapi_wrapper
+        self.stop_fn = stop_fn
+
+    @Slot()
+    def run(self):
+        """Screen Capture"""
+        self.autocapture = True
+        with mss.mss() as sct:
+            # Part of the screen to capture
+            monitor = {"top": 0, "left": 0, "width": self.screen_width, "height": self.screen_height}
+            prev_data = None
+
+            while "Screen capturing":
+
+                # Get raw pixels from the screen, save it to a Numpy array
+                sct_img = sct.grab(monitor)
+                pil_img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+                img = ImageQt.ImageQt(pil_img)
+                
+                # create the array from image
+                img_size = img.size()
+                buffer = img.constBits()
+                arr = np.ndarray(shape  = (img_size.height(), img_size.width(), img.depth()//8),
+                            buffer = buffer, 
+                            dtype  = np.uint8)
+                arr = arr[:,:,:3] # clip the A buffer off
+                
+                try:
+                    results, myartifact, ax, image = generate_dict(arr, self.screen_height, self.screen_width, citaw=self.citaw) # api
+                    # results,myartifact,ax,image = generate_dict(arr, currentHeight, currentWidth) # pytesseract
+                    print(results)
+                    print(myartifact)
+                    if myartifact != prev_data:
+                        self.new_artifact.emit(Artifact.fromDictionary(myartifact))
+                    prev_data = myartifact
+                except ValueError as e:
+                    print(e)
+                
+                if self.stop_fn():
+                    break
+                time.sleep(1)
+        
+        print('finished')
